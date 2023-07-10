25a76747b9cdb16b9bd89318b2c50e795b787906
Kunal Sabadra <85892457+Kunalsabadra@users.noreply.github.com>
2023-07-10 06:22:26 +0530

    Create 36-Flatten_LinkedList.cpp

---
+++ Linked_List/36-Flatten_LinkedList.cpp
@@ -0,0 +1,55 @@
+//Optimised Approach Using merge sort recursively
+/*
+    Time Complexity: O(n * n * k)
+    Space complexity: O(n * k)
+
+    Where 'n' denotes the size of the linked list and 'k' is the average number of child nodes for each of the n nodes.
+*/
+Node *merge(Node *first, Node *second)
+{
+
+    // If the first is NULL return second
+    if (first == NULL)
+    {
+        second->next = nullptr;
+        return second;
+    }
+
+    // If the second is NULL return first
+    if (second == NULL)
+    {
+        first->next = nullptr;
+        return first;
+    }
+
+    Node *merged = NULL;
+
+    if (first->data < second->data)
+    {
+        merged = first;
+        merged->child = merge(first->child, second);
+    }
+    else
+    {
+        merged = second;
+        merged->child = merge(first, second->child);
+    }
+    merged->next = nullptr;
+    return merged;
+}
+
+Node *flattenLinkedList(Node *head)
+{
+    if (head == NULL || head->next == NULL)
+    {
+        return head;
+    }
+
+    // Recur on next node
+    head->next = flattenLinkedList(head->next);
+
+    // Merge with the current
+    head = merge(head, head->next);
+
+    return head;
+}
