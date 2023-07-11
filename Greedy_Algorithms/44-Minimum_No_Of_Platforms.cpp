b618e8216ce3470636afe1e51ca8a28ee48c75fa
Kunal Sabadra <85892457+Kunalsabadra@users.noreply.github.com>
2023-07-11 08:11:09 +0530

    Update 44-Minimum_No_Of_Platforms.cpp

--- Greedy_Algorithms/44-Minimum_No_Of_Platforms.cpp
+++ Greedy_Algorithms/44-Minimum_No_Of_Platforms.cpp
@@ -1,1 +1,24 @@
+// Optimised Approach
+// Time Complexity: O(nlogn) Sorting takes O(nlogn) and traversal of arrays takes O(n) so overall time complexity is O(nlogn).
+// Space complexity: O(1)  (No extra space used).
 
+int calculateMinPatforms(int at[], int dt[], int n) {
+     int plat  = 1 , i =1 , j= 0 , result = 1;
+     sort(at , at+n);
+     sort(dt , dt+n);
+
+     while(i < n and j < n){
+          if(at[i] <= dt[j]){
+               plat++;
+               i++;
+          }
+          else if(at[i] > dt[j]){
+               plat--;
+               j++;
+          }
+          if(result < plat){
+               result = plat;
+          }
+     }
+      return result;
+}
