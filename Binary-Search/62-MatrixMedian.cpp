2268df662592d21fb174bc9f569801d28e0f7136
Kunal Sabadra <85892457+Kunalsabadra@users.noreply.github.com>
2023-07-14 15:28:47 +0530

    Create 62-MatrixMedian.cpp

---
+++ Binary-Search/62-MatrixMedian.cpp
@@ -0,0 +1,63 @@
+//Brute Force Approach
+// Time Complexity: O(row*col log(row*col)) for sorting the array where r*c denotes the number of elements in the linear array.
+// Space Complexity: O(row*col)  for storing elements in the linear array
+
+int Findmedian(int arr[3][3], int row, int col)
+{
+  int median[row * col];
+  int index = 0;
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < col; j++)
+    {
+      median[index] = arr[i][j];
+      index++;
+    }
+  }
+ 
+  return median[(row * col) / 2];
+}
+
+//Optimised Approach Using Binary Search
+// Time Complexity: O(row*log col) since the upper bound function takes log c time.
+// Space Complexity: O(1) since no extra space is required.
+
+
+int countSmallerThanMid(vector<int> &row, int mid)
+{
+  int l = 0, h = row.size() - 1;
+  while (l <= h)
+  {
+    int md = (l + h) >> 1;
+    if (row[md] <= mid)
+    {
+      l = md + 1;
+    }
+    else
+    {
+      h = md - 1;
+    }
+  }
+  return l;
+}
+int findMedian(vector<vector<int>> &A)
+{
+  int low = 1;
+  int high = 1e9;
+  int n = A.size();
+  int m = A[0].size();
+  while (low <= high)
+  {
+    int mid = (low + high) >> 1;
+    int cnt = 0;
+    for (int i = 0; i < n; i++)
+    {
+      cnt += countSmallerThanMid(A[i], mid);
+    }
+    if (cnt <= (n * m) / 2)
+      low = mid + 1;
+    else
+      high = mid - 1;
+  }
+  return low;
+}
