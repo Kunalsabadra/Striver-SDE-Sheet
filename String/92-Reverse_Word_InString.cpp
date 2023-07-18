2318c99765faf32a391b02eb25729a386d7c2920
Kunal Sabadra <85892457+Kunalsabadra@users.noreply.github.com>
2023-07-18 16:34:04 +0530

    Create 92-Reverse_Word_InString.cpp

---
+++ String/92-Reverse_Word_InString.cpp
@@ -0,0 +1,20 @@
+//Optimised Approach
+// Time Complexity: O(N), N~length of string
+// Space Complexity: O(1), Constant Space
+
+string reverseString(string &s){
+	string res;
+	int n = s.length();
+	int i =0;
+	while(i < n){
+		 while(i<n and s[i] == ' ')i++;
+		 if(i >= n) break;
+		 int j =i+1;
+		 while(j < n and s[j] != ' ') j++;
+		 string sub = s.substr(i, j-i);
+		 if(res.length() == 0) res = sub;
+		 else res = sub+" "+res;
+		 i=j+1;
+	}
+	return res;	
+}
