3aa5dbeda58a726053730f5beed3f232a68784f6
Kunal Sabadra <85892457+Kunalsabadra@users.noreply.github.com>
2023-07-25 18:20:01 +0530

    Create 139-Count_Distinct_Elements_inWindow_Size_k.cpp

---
+++ Heap/139-Count_Distinct_Elements_inWindow_Size_k.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h> 
+vector<int> countDistinctElements(vector<int> &arr, int k) 
+{
+   vector<int> ans;
+     int n = arr.size();
+    unordered_map<int , int> mp;
+
+    for(int i=0; i<k; i++){
+        mp[arr[i]]++;
+    }
+    ans.push_back(mp.size());
+
+    for(int i=k; i<n;i++){
+         if(mp[arr[i-k]] == 1){
+             mp.erase(arr[i-k]);
+         }
+         else{
+             mp[arr[i-k]]--;
+         }
+
+         mp[arr[i]]++;
+         ans.push_back(mp.size());
+    }  
+    return ans;
+	
+}
