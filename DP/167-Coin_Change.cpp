0246afab3a004d368a7e420fc3b1291c3d0a758e
Kunal Sabadra <85892457+Kunalsabadra@users.noreply.github.com>
2023-07-30 07:12:52 +0530

    Update 167-Coin_Change.cpp

--- DP/167-Coin_Change.cpp
+++ DP/167-Coin_Change.cpp
@@ -44,3 +44,27 @@ long countWaysToMakeChange(int *denominations, int n, int value)
 
 
 //Tabulation
+
+long countWaysToMakeChange(int *denominations, int n, int value)
+{     
+    vector<vector<long>> dp(n ,vector<long>(value+1 ,0));
+    //   return solve(n-1 , denominations , value ,dp);
+
+       for(int i=0; i<=value;i++){
+           if(i%denominations[0] == 0)
+            dp[0][i] = 1;
+            else dp[0][i] =0;
+       }
+
+        for(int i=1; i<n;i++){
+              for(int j=0; j<=value;j++){
+        long nottake  = dp[i-1][j];
+       long take = 0;
+       if(denominations[i] <= j){
+            take = dp[i][j - denominations[i]];
+                  }
+                dp[i][j] = take+nottake;
+              }
+        }
+        return dp[n-1][value];
+}
