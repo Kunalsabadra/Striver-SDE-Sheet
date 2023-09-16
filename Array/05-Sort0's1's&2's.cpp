b9817cc806b37d6cdb2834871cc0cfef5b3a48b1
Kunal Sabadra <85892457+Kunalsabadra@users.noreply.github.com>
2023-09-16 07:25:10 +0530

    Update 05-Sort0's1's&2's.cpp

--- Array/05-Sort0's1's&2's.cpp
+++ Array/05-Sort0's1's&2's.cpp
@@ -1,3 +1,43 @@
+//Brute Force Approach Using Inbuilt Sorting Functiom
+//T.C:O(NlogN) 
+//S.C:O(1)
+
+void sort012(vector<int> &nums){
+   sort(nums.begin() , nums.end())
+}
+/*-------------------------------------------------------------------------------*/
+//Better Approach : Mainting Count of three no's i.e 0 , 1 and 2
+//T.C:O(N)+O(N)
+//S.C:O(1)
+
+void sortColors(vector<int>& nums) {
+          int cnt0  = 0 , cnt1  = 0 , cnt2 = 0;
+
+          for(auto it:nums){
+               if(it  == 0) cnt0++;
+               else if(it == 1) cnt1++;
+               else cnt2++;
+          }
+
+          int i=0;
+          while(cnt0 > 0){
+               nums[i] = 0;
+               i++;
+               cnt0--;
+          }
+          while(cnt1 > 0){
+               nums[i] = 1;
+               i++;
+               cnt1--;
+          }
+          while(cnt2 > 0){
+               nums[i] = 2;
+               i++;
+               cnt2--;
+          }
+          return;
+    }
+/*---------------------------------------------------------------------------------------------------*/
 //Optimal Approach: Dutch National Flag ALGORITHM
 //T.C:0(N)
 //S.C:O(1)
