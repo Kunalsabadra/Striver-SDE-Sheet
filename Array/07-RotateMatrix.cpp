7b79f72099c3c3f7fe8a806b30d4e381138703ef
Kunal Sabadra <85892457+Kunalsabadra@users.noreply.github.com>
2023-09-22 07:48:35 +0530

    Update 07-RotateMatrix.cpp

--- Array/07-RotateMatrix.cpp
+++ Array/07-RotateMatrix.cpp
@@ -1,8 +1,11 @@
+//Rotate Matrix by 90deg Clockwise
 #include<bits/stdc++.h>
 
 //Optimal Approach:---------------------------------------------------------------------------------------------------
 //T.C: O(N*N) + O(N*N).      Where: One O(N*N) is for transposing the matrix and the other is for reversing the matrix.
 //S.C:O(1)
+
+//Take the transpose of the matrix and reverse each row of the matrix 
 void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
           
@@ -20,3 +23,43 @@ void rotate(vector<vector<int>>& matrix) {
            } 
 
     }
+// --------------------------------------------------------------------------------------------------------------------------------------
+//Rotate Matrix by 90 Deg Anticlockwise
+
+//Optimal Approach: Get the transpose of the matrix and reverse each column of the matrix
+//T.C: O(N*N) + O(N*N).      Where: One O(N*N) is for transposing the matrix and the other is for reversing the matrix.
+// S.C:O(1)
+
+#include<bits/stdc++.h>
+using namespace std;
+void rotate(vector<vector<int>>& matrix){
+        int n = matrix.size();
+
+//Reverse Eaach Row
+       for(int i=0; i<n;i++){
+          for(int j=0; j<=i;j++){
+              swap(matrix[i][j] , matrix[j][i]);
+          }
+       }
+
+     /* Reverse every column */
+  int ind = n - 1;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n / 2; j++) {
+      int temp = matrix[j][i];
+      matrix[j][i] = matrix[ind][i];
+      matrix[ind][i] = temp;
+      ind--;
+    }
+    ind = n - 1;
+  }
+}
+
+
+
+
+
+
+
+
+
