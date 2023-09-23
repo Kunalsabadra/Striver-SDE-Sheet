1038cf8b7048adee9160461387b1530bc83dcb8c
Kunal Sabadra <85892457+Kunalsabadra@users.noreply.github.com>
2023-09-23 09:16:05 +0530

    Update 10-FindDuplicates.cpp

--- Array/10-FindDuplicates.cpp
+++ Array/10-FindDuplicates.cpp
@@ -22,3 +22,22 @@ int findDuplicate(vector<int> &arr, int n){
 
 //Optimised Approach----------------------------------------------------------
 //Using Linkedlist Cycle method
+//T.C:O(N)
+//S.C:O(1)
+   int findDuplicate(vector<int>& nums) {
+          int n = nums.size();
+          int slow = nums[0];
+          int fast = nums[0];
+
+          do{
+               slow = nums[slow];
+               fast = nums[nums[fast]];
+          }while(slow != fast);
+
+         fast = nums[0];
+         while(slow != fast){
+              slow = nums[slow];
+              fast = nums[fast];
+         }
+        return slow;
+    }
